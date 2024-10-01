af440ed2cf9a8281b6c53e48f59b186ee89f2912
Sunshine Hughes <sunshinehughes2011@gmail.com>
2024-09-30 22:13:12 -0400

    changing nav bar to collapse

---
+++ script.js
@@ -0,0 +1,11 @@
+/* This is the navigation menu. This makes it collapse on a fullscreen */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  
+  
